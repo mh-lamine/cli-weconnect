9a29146a8100e8466c02435e104df6a5cc8918cc
mh-lamine <mlamins.ngom@gmail.com>
2024-08-09 18:13:45 +0200

    Add z-index to drawer-side in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -82,7 +82,7 @@ export default function Header() {
           </div>
         </div>
       </div>
-      <div className="drawer-side">
+      <div className="drawer-side z-10">
         <label
           htmlFor="my-drawer-3"
           aria-label="close sidebar"
